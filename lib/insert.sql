2d66c9f387d29b791e3e1da133de81ff29247ba0
stephenandersondev <stephenandersondev@gmail.com>
2020-09-03 19:27:45 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bear1", 1, "male", 'purple', 'calm', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bear2", 2, "male", 'green', 'sad', false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bear3", 3, "male", 'red', 'happy', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bear4", 4, "male", 'blue', 'excited', false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bear5", 5, "male", 'orange', 'grumpy', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bear6", 6, "male", 'yellow', 'tired', false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("bear7", 7, "male", 'pink', 'neutral', true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 8, "male", 'gray', 'goofy', false);
